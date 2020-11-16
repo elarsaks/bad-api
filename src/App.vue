e2dfb22580339f22a33e2751191c07a77b1b380c
elarsaks <elarsaks@gmail.com>
2020-11-16 20:58:49 +0200

    Add menu.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MenuButton
+      class="menu-button"
+      v-for="category in categories"
+      v-bind:key="category"
+      :name="category"
+      :selected="category == selected"
+      @change-category="changeCategory"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuButton from './components/MenuButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MenuButton,
+  },
+  data() {
+    return {
+      selected: 'shirts',
+      categories: ['jackets', 'shirts', 'accessories'],
+      loading: false,
+    }
+  },
+  methods: {
+    changeCategory(category){
+      this.selected = category
+      console.log(category)
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 25px;
 }
+
 </style>
---
+++ src/components/MenuButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <button 
+    :class="[ selected ? 'selected' : 'not-selected', hover ? 'hover' : '' ]"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+    @click="$emit('change-category', name)"
+    >
+    {{name}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'MenuButton',
+  props: {
+    name: String,
+    selected: Boolean,
+  },
+  data() {
+    return {
+      hover: false
+    }
+  },
+}
+</script>
+
+<style scoped>
+button {
+    padding: 1em;
+    margin: 0.5em;
+    font-size: 1.2em;
+    border-radius: 2vh;
+    border: none;
+    color: white;
+    text-transform: uppercase;
+    font-weight: 600;
+    z-index: 99;
+}
+
+button :hover{
+  cursor: pointer;
+}
+
+.selected {
+    background-color: rgb(86, 172, 0);
+}
+
+.not-selected {
+    background-color: rgb(185, 0, 40);
+}
+
+.hover{
+   cursor: pointer;
+}
+</style>
+
