fa11243cd15b354991b9d2294cdfdb16fcee66d2
elarsaks <elarsaks@gmail.com>
2020-12-28 19:21:19 +0200

    Add error handling for Redis connection.

--- data/src/repository/redis.js
+++ data/src/repository/redis.js
@@ -1,6 +1,10 @@
 const redis = require('redis')
 const client = redis.createClient(process.env.REDIS_URL)
 
+client.on("error", function(error) {
+    console.error(error);
+});
+
 const insertProductsByCategory = (category, data) => {
     return client.hmset(category, {
         category: category,
