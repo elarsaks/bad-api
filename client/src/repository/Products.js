7855c4b4745212bc4a6064e09e1a8507338c7b1e
elarsaks <elarsaks@gmail.com>
2021-01-01 20:42:27 +0200

    Change fetching url.

--- client/src/repository/Products.js
+++ client/src/repository/Products.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 function getProducts(category) {
   return axios({
-    url: `http://localhost:3001/products/${category}`,
+    url: `http://35.228.187.83:3001/products/${category}`,
     method: "GET",
   })
   .then(resp => resp.data)
