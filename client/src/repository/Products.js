0e133750e073a98f4a2f3d33e4f2ed0e6d519acc
elarsaks <elarsaks@gmail.com>
2021-01-02 05:14:11 +0200

    Change fetch link in client.

--- client/src/repository/Products.js
+++ client/src/repository/Products.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 function getProducts(category) {
   return axios({
-    //url: `http://35.228.187.83:3001/products/${category}`,
-    url: `http://localhost:3001/products/${category}`,
+    url: `http://35.228.187.83:3001/products/${category}`,
+    //url: `http://localhost:3001/products/${category}`,
     method: "GET",
   })
   .then(resp => resp.data)
